22e720417eefc4068a79efb1c4b764ce9e9ba829
Krishna Yerramsetty <krishna.yerramsetty@inscripta.com>
2024-04-16 05:57:31 +0000

    Adds BT_loss that uses DISCO gather functions to collect scores across GPUs. Without this only the samples within the current GPU are compared against each other

--- confit/stat_utils.py
+++ confit/stat_utils.py
@@ -3,6 +3,9 @@ import numpy as np
 import pandas as pd
 from scipy.stats import spearmanr
 from scipy import stats
+import torch.distributed as dist
+
+import disco
 
 
 def spearman(y_pred, y_true):
@@ -43,7 +46,7 @@ def compute_score(model, seq, mask, wt, pos, tokenizer):
     logits = out.logits
     log_probs = torch.log_softmax(logits, dim=-1)
     scores = torch.zeros(batch_size)
-    scores = scores.to(device)
+    # scores = scores.to(device)
 
     for i in range(batch_size):
 
@@ -51,21 +54,94 @@ def compute_score(model, seq, mask, wt, pos, tokenizer):
         score_i = log_probs[i]
         wt_i = wt[i]
         seq_i = seq[i]
+        # TODO: Does this compare across batches on different GPUs? If not, we need to fix this
         scores[i] = torch.sum(score_i[mut_pos+1, seq_i[mut_pos+1]])-torch.sum(score_i[mut_pos+1, wt_i[mut_pos+1]])
 
-    return scores, logits
+    return scores, logits, out, log_probs
 
 
-def BT_loss(scores, golden_score):
+def BT_loss(scores, golden_scores):
     loss = torch.tensor(0.)
     loss = loss.cuda()
+    
+    total_comparisons = 0
     for i in range(len(scores)):
-        for j in range(i, len(scores)):
-            if golden_score[i] > golden_score[j]:
+        for j in range(len(scores)):
+            total_comparisons += 1
+            if golden_scores[i] > golden_scores[j]:
                 loss += torch.log(1+torch.exp(scores[j]-scores[i]))
             else:
                 loss += torch.log(1+torch.exp(scores[i]-scores[j]))
     return loss
+
+
+# This function is useful for understanding how to implement gather from all GPUs but suffers from memory issues with activations not being removed from memory after every forward pass
+def BT_loss_disco(gpu, scores, golden_scores):
+    # Gather all scores from all GPUs
+    world_size = dist.get_world_size()
+    gathered_scores = [
+        torch.zeros_like(scores) for _ in range(world_size)
+    ]
+    gathered_golden_scores = [
+        torch.zeros_like(golden_scores) for _ in range(world_size)
+    ]
+    batch_size = scores.shape[0]
+
+    dist.all_gather(gathered_scores, scores)
+    dist.all_gather(gathered_golden_scores, golden_scores)
+    gathered_scores = torch.cat(gathered_scores)
+    gathered_golden_scores = torch.cat(gathered_golden_scores)
+    all_scores = gathered_scores.requires_grad_(True)
+    all_golden_scores = gathered_golden_scores
+
+    # Slice the scores and golden scores for the current GPU
+    scores = all_scores[batch_size*gpu:batch_size*(gpu+1)]
+    golden_scores = all_golden_scores[batch_size*gpu:batch_size*(gpu+1)]
+
+    loss = torch.tensor(0.)
+    loss = loss.cuda()
+    total_comparisons = 0
+    for i in range(len(scores)):
+        for j in range(len(all_scores)):
+            # print(i, j, scores[i], all_scores[j])
+            total_comparisons += 1
+            if golden_scores[i] > all_golden_scores[j]:
+                loss += torch.log(1+torch.exp(all_scores[j]-scores[i]))
+            else:
+                loss += torch.log(1+torch.exp(scores[i]-all_scores[j]))
+    
+    # Clean up
+    gathered_golden_scores = None
+    gathered_scores = None
+
+    return loss, scores, all_scores, all_golden_scores
+
+
+# Contrasts against all other sequences in the batch from all GPUs
+# This version works with removing activations from memory compared to the function above BT_loss_disco which doesn't
+def BT_loss_disco_gather(gpu, scores, golden_scores):
+    # Gather all scores from all GPUs
+    all_scores = disco.Gather(scores)
+    all_golden_scores = disco.Gather(golden_scores)
+
+    # Slice the scores and golden scores for the current GPU
+    batch_size = scores.shape[0]
+    scores = all_scores[batch_size*gpu:batch_size*(gpu+1)]
+    golden_scores = all_golden_scores[batch_size*gpu:batch_size*(gpu+1)]
+
+    loss = torch.tensor(0.)
+    loss = loss.cuda()
+    total_comparisons = 0
+    for i in range(len(scores)):
+        for j in range(len(all_scores)):
+            # print(i, j, scores[i], all_scores[j])
+            total_comparisons += 1
+            if golden_scores[i] > all_golden_scores[j]:
+                loss += torch.log(1+torch.exp(all_scores[j]-scores[i]))
+            else:
+                loss += torch.log(1+torch.exp(scores[i]-all_scores[j]))
+    print(f"Total comparisons: {total_comparisons}")
+    return loss, scores, all_scores, all_golden_scores
 
 
 def KLloss(logits, logits_reg, seq, att_mask):
@@ -88,5 +164,7 @@ def KLloss(logits, logits_reg, seq, att_mask):
         reg = probs_reg_i[torch.arange(0, seq_len), seq[i, :seq_len]]
         pred = probs_i[torch.arange(0, seq_len), seq[i, :seq_len]]
 
-        loss += creterion_reg(reg.log(), pred)
+        # TODO: Should the inputs be reversed here? See the definition of KLDivLoss from torch.nn and make sure we are using the reference distribution in the denominator
+        # loss += creterion_reg(reg.log(), pred)  # This is the original loss as implemented in the code for ConFit
+        loss += creterion_reg(pred.log(), reg)
     return loss
